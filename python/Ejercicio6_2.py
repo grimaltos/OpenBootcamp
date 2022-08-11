4b658a276987388d57d1fa8221cd5360c8c03a2f
Vicent Grimaltos <vicent@grimaltos.com>
2022-08-11 18:19:40 +0200

    Corregido ejercicio 6.2

--- python/Ejercicio6_2.py
+++ python/Ejercicio6_2.py
@@ -1,26 +1,26 @@
 class Alumno:
-    def __init__(self, nombre):
+    def inicializar(self, nombre, nota):
         self.nombre = nombre
-        self.notas={}
+        self.nota = nota
 
-    def setNota(self, asignatura, nota):
-        self.notas.update({asignatura: nota})
+    def imprimir(self):
+        print("Nombre: ", self.nombre)
+        print("Nota: ", self.nota)
 
-    def dameNotas(self):
-        return self.notas
 
-    def __str__(self):
-        return "Notas de {}".format(self.nombre)
+    def resultado(self):
+        if self.nota >= 5:
+            print("Aprobado")
+        else:
+            print("Suspenso")
 
-vicent = Alumno("Vicent")
+a1 = Alumno()
+a2 = Alumno()
 
-vicent.setNota("Python", 10)
-vicent.setNota("Java", 6)
-print(vicent)
-notas = vicent.dameNotas()
-for x, y in notas.items():
-    print(x, ' --> ', y)
-    if(y >= 5):
-        print("APROBADA")
-    else:
-        print("SUSPENSA")
+a1.inicializar("Vicent", 10)
+a2.inicializar("Merche", 4.9)
+
+a1.imprimir()
+a1.resultado()
+a2.imprimir()
+a2.resultado()
