b594fba9df1c26041e60793a36bb970265a3940e
Vicent Grimaltos <vicent@grimaltos.com>
2022-09-07 21:35:42 +0200

    modified:   JavaScrpt/Ejercicio2/index.js

--- JavaScrpt/Ejercicio2/index.js
+++ JavaScrpt/Ejercicio2/index.js
@@ -1,12 +1,8 @@
-const nombre = 'Vicent';
-let edad = 35;
-let desarrollador = false;
-const fechanacimiento = Date('28/01/1987');
-const librofavorito ={
+const lisa = ['Vicent',35,false, new Date(1987,0,28),
+{
     titulo: 'PERSONAS DECENTES',
     autor: 'LEONARDO PADURA',
     fecha: '30/08/2022',
     url: 'https://www.casadellibro.com/libro-esclava-de-la-libertad/9788425361791/13073829'
 }
-
-console.log(typeof(fechanacimiento));
+];
